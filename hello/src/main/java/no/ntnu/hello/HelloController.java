8f69e0072ca31c7e3ed7155f367584c10a295132
Haukaun <haakonfs@hotmail.com>
2022-01-13 18:33:49 +0100

    Excerise week1

--- hello/src/main/java/no/ntnu/hello/HelloController.java
+++ hello/src/main/java/no/ntnu/hello/HelloController.java
@@ -1,4 +1,13 @@
 package no.ntnu.hello;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String greeting(){
+        return "Christian liker aids på pinne";
+    }
 }
