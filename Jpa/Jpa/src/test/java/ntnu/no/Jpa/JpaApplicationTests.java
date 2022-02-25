a367b020d88b1914b7001e37897137230de65a52
Haukaun <haakonfs@hotmail.com>
2022-02-25 03:21:40 +0100

    Working with docker compose and dockerfile. Setup complete.

--- Jpa/Jpa/src/test/java/ntnu/no/Jpa/JpaApplicationTests.java
+++ Jpa/Jpa/src/test/java/ntnu/no/Jpa/JpaApplicationTests.java
@@ -3,10 +3,10 @@ package ntnu.no.Jpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class JpaApplicationTests {
 
-	@Test
+
 	void contextLoads() {
 	}
 
